c31460b3048d338b8989777c279badf04b4968cf
fanfan <fanfan0521@yeah.net>
2025-11-16 22:45:59 +0800

    refactor(router): 将路由组件改为懒加载形式并重构路由映射
    - 将直接导入的路由组件改为动态导入形式，优化首屏加载性能
    - 重构路由映射表为ROUTE_MAP，并添加异常组件映射
    - 在权限模块中添加wrapperRouteComponent方法动态设置路由组件
    - 修改权限过滤逻辑以适配新的路由映射结构

--- src/router/router-map.ts
+++ src/router/router-map.ts
@@ -1,9 +1,13 @@
 import { LAYOUT } from '@/router/constant';
-import Analysis from '@/views/dashboard/analysis/index.vue';
-import Workbench from '@/views/dashboard/workbench/index.vue';
 
-export const MAP = {
+// 将直接导入改为懒加载形式
+export const Analysis = () => import('@/views/dashboard/analysis/index.vue');
+export const Workbench = () => import('@/views/dashboard/workbench/index.vue');
+export const EXCEPTION_COMPONENT = () => import('@/views/sys/exception/Exception.vue');
+
+export const ROUTE_MAP = {
   Dashboard: LAYOUT,
   Analysis: Analysis,
   Workbench: Workbench,
+  EXCEPTION_COMPONENT: EXCEPTION_COMPONENT,
 };
--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -24,6 +24,8 @@ import { getPermCode } from '@/api/sys/user';
 
 import { useMessage } from '@/hooks/web/useMessage';
 import { PageEnum } from '@/enums/pageEnum';
+import { constant } from 'lodash-es';
+import { ROUTE_MAP } from '@/router/router-map';
 
 interface PermissionState {
   // Permission code list
@@ -179,6 +181,19 @@ export const usePermissionStore = defineStore({
         }
         return;
       };
+      const wrapperRouteComponent = (routes) => {
+        return routes.map((route) => {
+          // 如果有子路由  进行递归遍历
+          if (route.children && route.children.length > 0) {
+            route.children = wrapperRouteComponent(route.children);
+          } else {
+            // route.component = () => import('@/views/dashboard/analysis/index.vue');
+            route.component = ROUTE_MAP[route.name as string] || ROUTE_MAP.EXCEPTION_COMPONENT;
+          }
+          return route;
+        });
+      };
+
       const backendRoutes = JSON.stringify({
         path: '/dashboard',
         name: 'Dashboard',
@@ -216,7 +231,7 @@ export const usePermissionStore = defineStore({
           // 设置菜单列表
           this.setStaticMenuList(staticMenuList);
           // 对非一级路由进行过滤
-          routes = filter(backendRoutes, routeFilter);
+          routes = filter(wrapperRouteComponent(JSON.parse(backendRoutes)), routeFilter);
           // 对一级路由根据角色权限过滤
           routes = routes.filter(routeFilter);
           // Convert multi-level routing to level 2 routing
@@ -227,7 +242,7 @@ export const usePermissionStore = defineStore({
         // 路由映射， 默认进入该case
         case PermissionModeEnum.ROUTE_MAPPING:
           // 对非一级路由进行过滤
-          routes = filter(backendRoutes, routeFilter);
+          routes = filter(wrapperRouteComponent([JSON.parse(backendRoutes)]), routeFilter);
           // 对一级路由再次根据角色权限过滤
           routes = routes.filter(routeFilter);
           // 将路由转换成菜单
